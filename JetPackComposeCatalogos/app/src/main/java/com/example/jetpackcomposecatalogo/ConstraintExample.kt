07145c1729f4c6d56e64e1636f8336c5612975c1
Diego Enrique <desarrollo.software5@prosur.com.mx>
2024-09-17 18:54:45 -0600

    ConstraintLayout2

--- JetPackComposeCatalogos/app/src/main/java/com/example/jetpackcomposecatalogo/ConstraintExample.kt
+++ JetPackComposeCatalogos/app/src/main/java/com/example/jetpackcomposecatalogo/ConstraintExample.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-@Preview
+
 @Composable
 fun ConstraintExample1(){
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
@@ -19,29 +19,83 @@ fun ConstraintExample1(){
 
 
 
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                top.linkTo(boxRed.bottom)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
+
+
+@Composable
+fun ConstraintExampleGuide(){
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val boxRed = createRef()
+//        val starGuide = createGuidelineFromTop(16.dp)
+        val topGuide = createGuidelineFromTop(0.1f)
+        val starGuide = createGuidelineFromStart(0.25f)
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(topGuide)
+                start.linkTo(starGuide)
+            })
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintBarrier(){
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (boxRed,boxGreen,boxYellow) = createRefs()
+        val  barrier = createEndBarrier(boxRed,boxGreen)
+
         Box(modifier = Modifier.size(125.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(parent.top)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start, margin = 16.dp)
         })
-        Box(modifier = Modifier.size(125.dp).background(Color.Blue).constrainAs(boxBlue){
+        Box(modifier = Modifier.size(235.dp).background(Color.Green).constrainAs(boxGreen){
             top.linkTo(boxRed.bottom)
-            end.linkTo(boxRed.start)
+            start.linkTo(parent.start, margin = 32.dp)
         })
-
-        Box(modifier = Modifier.size(125.dp).background(Color.Yellow).constrainAs(boxYellow){
-            bottom.linkTo(boxRed.top)
-            end.linkTo(boxRed.start)
-        })
-
-        Box(modifier = Modifier.size(125.dp).background(Color.Magenta).constrainAs(boxMagenta){
-            bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.end)
-        })
-        Box(modifier = Modifier.size(125.dp).background(Color.Green).constrainAs(boxGreen){
-            top.linkTo(boxRed.bottom)
-            start.linkTo(boxRed.end)
+        Box(modifier = Modifier.size(50.dp).background(Color.Yellow).constrainAs(boxYellow){
+            start.linkTo(barrier)
         })
     }
 }
